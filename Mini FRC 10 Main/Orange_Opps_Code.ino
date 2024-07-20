7602bcee3160084496573b339b2411b4f9beddc9
Ben Park <152503119+benPark20@users.noreply.github.com>
2024-07-19 22:07:45 -0400

    needs tuning

--- Mini FRC 10 Main/Orange_Opps_Code.ino
+++ Mini FRC 10 Main/Orange_Opps_Code.ino
@@ -23,114 +23,126 @@ int AUTO_CANCEL_BUTTON = 9;
 
 // This stores the time at which we started auto mode. This lets us keep track of how long we've been in auto mode for.
 long AUTO_START_TIME = 0;
+long SHOOTER_START_TIME = 0;
 State ROBOT_STATE;
 
 void setup() {
-	//EVERYONE SHOULD CHANGE "ESP32 Bluetooth" TO THE NAME OF THEIR ROBOT
-	PestoLink.begin("CallingAllBluds");
-	// If a motor in your drivetrain is spinning the wrong way, change the value for it here from 'false' to 'true'
-    frontLeftMotor.setInverted(true);
-    frontRightMotor.setInverted(false);
-    rearLeftMotor.setInverted(true);
-    rearRightMotor.setInverted(false);
+  //EVERYONE SHOULD CHANGE "ESP32 Bluetooth" TO THE NAME OF THEIR ROBOT
+  PestoLink.begin("CallingAllBluds");
+  // If a motor in your drivetrain is spinning the wrong way, change the value for it here from 'false' to 'true'
+  frontLeftMotor.setInverted(true);
+  frontRightMotor.setInverted(false);
+  rearLeftMotor.setInverted(true);
+  rearRightMotor.setInverted(false);
 
-	// No need to mess with this code
-    RSL::initialize();
-    RSL::setState(RSL_ENABLED);
-    ROBOT_STATE = MANUAL;
+  // No need to mess with this code
+  RSL::initialize();
+  RSL::setState(RSL_ENABLED);
+  ROBOT_STATE = MANUAL;
 }
 
 void loop() {
-	// Here we define the variables we use in the loop
-    float indexerThrottle = 0;
-    float shooterThrottle = 0.1; //this needs tuning
-    int servoAngle = 90;
-    long SHOOTER_START_TIME = 0;
+  // Here we define the variables we use in the loop
+  float indexerThrottle = 0;
+  float shooterThrottle = 0.1; //this needs tuning
+  int servoAngle = 90;
 
-	//When PestoLink.update() is called, it returns "true" only if the robot is connected to phone/laptop  
-    if (PestoLink.update()) {
-      if (ROBOT_STATE == MANUAL) {
-        // We only want to check manual controls if we're in manual mode!
-          if (PestoLink.buttonHeld(AUTO_START_BUTTON)) {
-            // If the auto mode button is pressed, we should switch to auto mode.
-            ROBOT_STATE = AUTO;
-            AUTO_START_TIME = millis();
-            return; // We don't want to check the manual controls - we just switched to auto mode! This immediately restarts the loop.
-          }
-          float xVelocity = PestoLink.getAxis(0) * 1;
-          float yVelocity = -PestoLink.getAxis(1) * 1;
-          float rotation = PestoLink.getAxis(2) * 1;
+  //When PestoLink.update() is called, it returns "true" only if the robot is connected to phone/laptop  
+  if (PestoLink.update()) {
+    if (ROBOT_STATE == MANUAL) {
+      // We only want to check manual controls if we're in manual mode!
+      if (PestoLink.buttonHeld(AUTO_START_BUTTON)) {
+        // If the auto mode button is pressed, we should switch to auto mode.
+        ROBOT_STATE = AUTO;
+        AUTO_START_TIME = millis();
+        return; // We don't want to check the manual controls - we just switched to auto mode! This immediately restarts the loop.
+      }
+      float xVelocity = PestoLink.getAxis(0) * 1;
+      float yVelocity = -PestoLink.getAxis(1) * 1;
+      float rotation = PestoLink.getAxis(2) * 1;
 
-          drivetrain.holonomicDrive(xVelocity, yVelocity, rotation);
+      drivetrain.holonomicDrive(xVelocity, yVelocity, rotation);
 
       // Here we decide what the servo angle will be based on if a button is pressed ()
 
       // Servo Code:
+      float shootSpeed;
       if (PestoLink.buttonHeld(3)) {
-        servoAngle = 150; //climber up angle;
+        servoAngle = 140; //climber up angle/amp
         servo.write(servoAngle);
-      }
-      else if (PestoLink.buttonHeld(0)){
-        servoAngle = 30; //intake angle
+        shootSpeed = -0.75;
+      } else if (PestoLink.buttonHeld(0)) {
+        servoAngle = 29; //subwoofer angle
         servo.write(servoAngle);
-      }
-      else if (PestoLink.buttonHeld(2)){
-        servoAngle = 95; //passing
+        shootSpeed = -0.75;
+      } else if (PestoLink.buttonHeld(2)) {
+        servoAngle = 60; //passing
+        shootSpeed = -0.65;
         servo.write(servoAngle);
-      }
-      else if (PestoLink.buttonHeld(1)){
-        servoAngle = servoAngle + 10;
+      } else if (PestoLink.buttonHeld(1)) {
+        servoAngle = 53; //podium
         servo.write(servoAngle);
       }
       
       // Motor Code:
       if (PestoLink.buttonHeld(4)) {
         indexerThrottle = 1;
-      } 
-      else if (PestoLink.buttonHeld(6)){
+        servo.write(-9);
+      } else if (PestoLink.buttonHeld(6)) {
         indexerThrottle = -1;
-      } 
-      else {
+        shooterThrottle = -0.75;
+      } else {
         indexerThrottle = 0;
       }
+      
       if (PestoLink.buttonHeld(5)) {
-        shooterThrottle = 0.75;
-      } 
-      else if (PestoLink.buttonHeld(7)){
-        shooterThrottle = -0.75;
-      } 
-      else {
+        shooterThrottle = 0.9;
+      } else if (PestoLink.buttonHeld(7)) {
+        shooterThrottle = -1;
+        SHOOTER_START_TIME = millis();
+      } else {
         shooterThrottle = 0;
       }
-      indexerMotor.set(indexerThrottle);
-      shooterMotor.set(shooterThrottle);
-      } //write loop code above this braket!!!
-      else {
-        // We're in auto mode, so we should handle auto mode.
-        if (PestoLink.update()) {
-          if (PestoLink.buttonHeld(AUTO_CANCEL_BUTTON)) {
-            // Check to see if we should cancel auto mode.
-            ROBOT_STATE = MANUAL;
-            return;
-          }
-        }
-        // If it's been less than one second (or, 1000 milliseconds) since we started auto mode, drive forwards.
-        if ((millis() - AUTO_START_TIME) < 1000) {
-          drivetrain.arcadeDrive(-1, 0);
-        }
-        // Otherwise, stop and exit auto mode.
-        else {
-          drivetrain.arcadeDrive(0,0);
-          ROBOT_STATE = MANUAL;
-          return;
+      
+      // Handle indexer motor timing based on SHOOTER_START_TIME
+      if (SHOOTER_START_TIME != 0) {
+        unsigned long currentTime = millis();
+        unsigned long elapsedTime = currentTime - SHOOTER_START_TIME;
+        if (elapsedTime > 1 && elapsedTime <= 1250) {
+          indexerThrottle = 1;
+        } else if (elapsedTime > 1500) {
+          SHOOTER_START_TIME = 0; // Reset SHOOTER_START_TIME after the interval
         }
       }
 
-        RSL::setState(RSL_ENABLED);
+      indexerMotor.set(indexerThrottle);
+      shooterMotor.set(shooterThrottle);
     } else {
-        RSL::setState(RSL_DISABLED);
+      // We're in auto mode, so we should handle auto mode.
+      if (PestoLink.buttonHeld(AUTO_CANCEL_BUTTON)) {
+        // Check to see if we should cancel auto mode.
+        ROBOT_STATE = MANUAL;
+        return;
+      }
+      // If it's been less than one second (or, 1000 milliseconds) since we started auto mode, shoot.
+      if ((millis() - AUTO_START_TIME) < 1000) {
+        servo.write(29);
+        shooterMotor.set(-0.8);
+        SHOOTER_START_TIME = millis();
+      } 
+      // Otherwise, stop and exit auto mode.
+      else {
+        drivetrain.arcadeDrive(0, 0);
+        ROBOT_STATE = MANUAL;
+        return;
+      }
     }
 
-	// No need to mess with this code
-    RSL::update();
+    RSL::setState(RSL_ENABLED);
+  } else {
+    RSL::setState(RSL_DISABLED);
+  }
+
+  // No need to mess with this code
+  RSL::update();
 }
